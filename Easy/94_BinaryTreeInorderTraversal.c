3612de49a92aaf4df5bb9faaef76e35aef29d8d6
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-05-02 15:18:46 +0900

    Add files via upload

---
+++ Easy/94_BinaryTreeInorderTraversal.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int getTreeSize(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    return 1 + getTreeSize(root->left) + getTreeSize(root->right);
+}
+
+void fill(struct TreeNode* root, int* arr, int* index) {
+    if (root == NULL) return;
+
+    fill(root->left, arr, index);
+    arr[(*index)++] = root->val;
+    fill(root->right, arr, index);
+}
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int size = getTreeSize(root);    
+    *returnSize = size;
+
+    int* result = (int *)malloc(sizeof(int) * size);
+    int index = 0;
+    fill(root, result, &index);
+
+    return result;
+}
