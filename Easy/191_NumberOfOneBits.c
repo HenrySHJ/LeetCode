43c3df92db71f7d1b22d5ab11d791a6f8b225e2e
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-08-11 12:16:26 +0900

    Add files via upload

---
+++ Easy/191_NumberOfOneBits.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int hammingWeight(int n) {
+    int ans = 0;
+    
+    while (n) {
+        if (n & 1 == 1)
+            ans++;
+        n = n >> 1;
+    }
+
+    return ans;
+}
