b04e16fe623f0274f72ff223b8d46f00cea178fb
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-08-18 15:10:14 +0900

    Add files via upload

---
+++ Easy/268_MissingNumber.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int missingNumber(int* nums, int numsSize) {
+    int sum = (numsSize * (numsSize + 1)) / 2;
+
+    for (int i = 0; i < numsSize; i++)
+        sum -= nums[i];
+
+    return sum;
+}
