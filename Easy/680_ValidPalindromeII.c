92dca14a398a37bd7cb0b4122a008912b2d19bb3
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-08-26 15:40:19 +0900

    Add files via upload

---
+++ Easy/680_ValidPalindromeII.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdbool.h>
+
+bool isPalindrome(char* s, int l, int r) {
+    while (l < r) {
+        if (s[l] != s[r]) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+
+bool validPalindrome(char* s) {
+    int l = 0;
+    int r = strlen(s) - 1;
+
+    while (l < r) {
+        if (s[l] != s[r]) 
+            return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+        l++;
+        r--;
+    }
+
+    return true;
+}
